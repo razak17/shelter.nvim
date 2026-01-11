cab1bce7306ae36b477fe9ad791b925adec7069b
ph1losof <my.tentacles.are.in.you@gmail.com>
2026-01-11 14:56:04 +0100

    feat: use 10k iterations for benchmarking

--- benchmarks/benchmark.lua
+++ benchmarks/benchmark.lua
@@ -8,7 +8,7 @@
 -- 2. Preview: Masking a preview buffer (Telescope scenario)
 -- 3. Edit: Re-masking after text change (typing scenario)
 
-local ITERATIONS = 1000
+local ITERATIONS = 10000
 local SIZES = { 10, 50, 100, 500 }
 local OUTPUT_FILE = "benchmark_results.json"
 
